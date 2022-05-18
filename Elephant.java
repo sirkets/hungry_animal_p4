11e1109408497e41e8f6cc1eafc726ee07430344
Stewart Chan <stew.chan@gmail.com>
2022-05-18 13:27:51 -0400

    resized, turned and slowed down the elephant

--- Elephant.java
+++ Elephant.java
@@ -2,32 +2,51 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
 {
-    GreenfootImage[] images;
+    private GreenfootImage[] rightImages;
+    private GreenfootImage[] leftImages;
+    private SimpleTimer timer;
+    private boolean isFacingRight = true;
     
     public Elephant()
     {
-        images = new GreenfootImage[8];
-        for(int i = 0; i < images.length; i++){
-            images[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        rightImages = new GreenfootImage[8];
+        leftImages = new GreenfootImage[8];
+        for(int i = 0; i < rightImages.length; i++){
+            rightImages[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            rightImages[i].scale(100, 100);
+            leftImages[i] =new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            leftImages[i].scale(100,100);
+            leftImages[i].mirrorHorizontally();
         }
-        setImage(images[4]);
+        setImage(rightImages[0]);
+        timer = new SimpleTimer();
+        timer.mark();
     }
     
     int curIndex = 0;
     void animate()
     {
-        setImage(images[curIndex]);
-        curIndex++;
-        curIndex %= 8;
+        if(timer.millisElapsed() > 100){
+            if(isFacingRight) {
+                setImage(rightImages[curIndex]);
+            } else {
+                setImage(leftImages[curIndex]);
+            }
+            curIndex++;
+            curIndex %= 8;
+            timer.mark();
+        }
     }
     
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("a")){
+            isFacingRight = false;
             move(-2);
         }
         if(Greenfoot.isKeyDown("d")){
+            isFacingRight = true;
             move(2);
         }
         if(Greenfoot.isKeyDown("space")){
